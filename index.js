26d1321d0414d529961386c892779b324510c435
Warren Yao <warrenyca905@gmail.com>
2020-02-02 20:27:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,8 +17,9 @@ class Polygon {
 class Triangle extends Polygon {
   get isValid() {
     if(this.countSides === 3) {
-      if(this.array[0] + this.array[1] > this.array[2])
-      return true
+      if(this.array[0] + this.array[1] > this.array[2]) {
+        return true
+      }
     }
     return false
   }
